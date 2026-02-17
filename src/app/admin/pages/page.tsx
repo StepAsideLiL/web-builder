6cba7874d54eaf71798de306435b95f0310dc8c5
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2026-02-17 12:11:42 +0600

    show all pages.

--- src/app/admin/pages/page.tsx
+++ src/app/admin/pages/page.tsx
@@ -35,10 +35,12 @@ export default async function Page(props: PageProps<"/admin/pages">) {
       .where(eq(pagesTable.slug, slug))
       .get();
 
-    if (pageContent) return <EditPageContent content={pageContent} />;
+    if (pageContent) {
+      return <EditPageContent content={pageContent} />;
+    }
   }
 
-  const pageContent = await db.select().from(pagesTable);
+  const pages = await db.select().from(pagesTable);
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-5 py-10">
@@ -49,8 +51,22 @@ export default async function Page(props: PageProps<"/admin/pages">) {
       </section>
 
       <section>
-        {pageContent.length !== 0 ? (
-          <div>pages</div>
+        {pages.length !== 0 ? (
+          <div className="space-y-2">
+            {pages.map((page) => (
+              <div key={page.id} className="border p-2">
+                <h1>{page.title}</h1>
+                <Button variant={"link"} asChild>
+                  <Link href={`/admin/pages?action=edit&slug=${page.slug}`}>
+                    Edit
+                  </Link>
+                </Button>
+                <Button variant={"link"} asChild>
+                  <Link href={`/${page.slug}`}>View</Link>
+                </Button>
+              </div>
+            ))}
+          </div>
         ) : (
           <div>
             <Empty className="border shadow">
